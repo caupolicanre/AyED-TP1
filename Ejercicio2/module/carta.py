7011496e91b8cfe49cc998c4af8d4ec24e3cd9db
Caupolicán Ré <caupolicanre@gmail.com>
2022-09-09 17:09:32 -0300

    Creo clase "Carta" y defino sus atributos iniciales

---
+++ Ejercicio2/module/carta.py
@@ -0,0 +1,36 @@
+from nodo import Nodo
+
+class Carta(Nodo):
+    
+    def __init__(self, palo, numero):
+        self._palo = palo
+        self._numero = numero
+        self.boca_abajo = None
+        self.boca_arriba = None
+    
+
+    # Propiedades
+    
+    @property
+    def palo(self):
+        return self._palo
+    
+    @property
+    def numero(self):
+        return self._numero
+    
+    @property
+    def boca_abajo(self):
+        return self.boca_abajo
+    
+    @boca_abajo.setter
+    def boca_abajo(self, estado):
+        self.boca_abajo = estado
+
+    @property
+    def boca_arriba(self):
+        return self.boca_arriba
+    
+    @boca_arriba.setter
+    def boca_arriba(self, estado):
+        self.boca_arriba = estado
