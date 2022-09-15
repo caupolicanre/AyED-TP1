2c5827f2117762c010ca18d2c539f5ebec76e5ba
Caupolicán Ré <caupolicanre@gmail.com>
2022-09-15 14:21:38 -0300

    Actualizo importacion

--- Ejercicio2/modulos/cola_doble.py
+++ Ejercicio2/modulos/cola_doble.py
@@ -1,13 +1,13 @@
-from modulos.LDE import ListaDoble
+from modulos.LDE import ListaDobleEnlazada
 from carta import Carta
 
-class ColaDoble(ListaDoble):
+class ColaDoble(ListaDobleEnlazada):
     
     def encolar(self, item):
-        ListaDoble.agregar(item)
+        ListaDobleEnlazada.agregar(item)
         
     def desencolar(self):
-        ListaDoble.extraer(ListaDoble.tamanio-1)
+        ListaDobleEnlazada.extraer(ListaDobleEnlazada.tamanio-1)
 
 pruebaColaDoble= ColaDoble()
 print(pruebaColaDoble.esta_vacia())
