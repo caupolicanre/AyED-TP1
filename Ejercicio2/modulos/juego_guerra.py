a226284714939ef4913bf2d6ab6700101f9439ce
Caupolicán Ré <caupolicanre@gmail.com>
2022-09-28 19:02:33 -0300

    Avances juego guerra

--- Ejercicio2/modulos/juego_guerra.py
+++ Ejercicio2/modulos/juego_guerra.py
@@ -1,16 +1,20 @@
-
 from cola_doble import ColaDoble
+from mazo import Mazo
+import random as rd
 
 class JuegoGuerra:
 
-    def __init__(self, random_seed, cant_turnos):
-        self.cantidad_turnos = cant_turnos 
-        self.turnos_jugados = 0
-        self.ganador = None
-        self.empate = None
-
-        self.mazo_jugador1 = ColaDoble()
-        self.mazo_jugador2 = ColaDoble()
+    def __init__(self, seed):
+        # self.cantidad_turnos = cant_turnos 
+        # self.turnos_jugados = 0
+        # self.ganador = None
+        # self.empate = None
+        
+        self.mazo_mesa = Mazo()
+        self.mazo_jugador1 = Mazo()
+        self.mazo_jugador2 = Mazo()
+        
+        self.semilla = seed
         
     
     # Propiedades
@@ -19,7 +23,17 @@ class JuegoGuerra:
     def turnos_jugados(self):
         return self._turnos_jugados
     
+    def crear_mazo(self):
+        None
 
-
+    def repartir(self):
+        for i, carta in enumerate(self.mazo_mesa):
+            if i%2 == 0:
+                self.mazo_jugador1.agregar_carta(carta)
+            if i%2 != 0:
+                self.mazo_jugador2.agregar_carta(carta)
+        return mazo1 y mazo2.
     def iniciar_juego(self):
         None
+    
+    def 
---
+++ Ejercicio2/modulos/mazo.py
@@ -0,0 +1,20 @@
+from cola_doble import ColaDoble
+
+class Mazo:
+    
+    def __init__(self):
+        self.mazo = ColaDoble
+    
+    def __str__(self):
+        return str(self.mazo)
+    
+    def __iter__(self):
+        return iter(self.mazo)
+    
+    def agregar_carta(self, cartaNueva):
+        None
+
+    def jugar_carta(self, estadoCara="Boca abajo"):
+        None
+    
+    def jugador_gana(self, )
