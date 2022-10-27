ffd4c6afcd4e2bcd0cfa5f22fea9b0ec19fb7e38
caupolicanre <caupolicanre@gmail.com>
2022-10-27 08:04:16 -0300

    Agrego atributos, modifico método repartir, y crreo el método iniciar_juego

--- Ejercicio2/modulos/juego_guerra.py
+++ Ejercicio2/modulos/juego_guerra.py
@@ -5,33 +5,24 @@ import random as rd
 
 class JuegoGuerra:
 
-    def __init__(self, random_seed):
-        # self.cantidad_turnos = cant_turnos 
-        # self.turnos_jugados = 0
-        # self.ganador = None
-        # self.empate = None
+    def __init__(self, random_seed=22):
         
         self.mazo = Mazo()
         self.mazo_jugador1 = Mazo()
         self.mazo_jugador2 = Mazo()
-        
+        self.n_jugadores = 2
+        self.valores = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+        self.palos = ['♠', '♥', '♦', '♣']
         self._semilla = random_seed
+        
+        self.cantidad_turnos = None
+        self.turnos_jugados = 0
+        self.empate = False
+        
+        self.ganador = None
         
     
     # Propiedades
-
-    @property
-    def semilla(self):
-        '''
-        Getter de semilla.
-
-        Returns
-        -------
-        int
-            Retorna la semilla con la que se inicializó el mazo.
-
-        '''
-        return self._semilla
 
     # @property
     # def turnos_jugados(self):
@@ -51,28 +42,24 @@ class JuegoGuerra:
             El mazo está hecho con una Cola Doble.
 
         '''
-        
-        valores = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-        palos = ['♠', '♥', '♦', '♣']
+
         cartas = []     # Lista para guardar las cartas, mezclarlas y luego crear un mazo
         jerarquia = 0   # Utilizo una jerarquía para comparar luego las cartas
         
         # Asigno cada número a los 4 palos
-        for numero in valores:
+        for numero in self.valores:
             jerarquia+=1    # Aumento la jerarquía por cada iteración
             
-            for palo in palos:
+            for palo in self.palos:
                 carta = Carta(palo, numero, jerarquia, "Boca abajo")  # Creo una carta y le paso los parámetros correspondientes
                 cartas.append(carta)    # Agrego la carta a la lista de cartas para crear luego el mazo
         
-        rd.seed(self.semilla)   # Asigno una semilla para que se mezcle de una forma en específico
+        rd.seed(self._semilla)   # Asigno una semilla para que se mezcle de una forma en específico
         rd.shuffle(cartas)      # Mezclo las cartas
         
         # Una vez mezcladas las cartas, las agrego al mazo
         for carta in cartas:
             self.mazo.agregar_carta(carta)
-        
-        return self.mazo
 
 
     def repartir(self):
@@ -82,16 +69,14 @@ class JuegoGuerra:
 
         Returns
         -------
-        class
-            Retorna mazo del Jugador 1.
-            Creado con una Cola Doble.
-        class
-            Retorna mazo del Jugador 2.
-            Creado con una Cola Doble.
+        None.
 
         '''
         
-        for i, carta in enumerate(self.mazo):
+        nCartas = self.mazo.tamanio()
+        print(nCartas)
+        
+        for i in range(nCartas):
             '''
             Si "i" es par, la carta va al mazo del Jugador 1,
             si es impar, la carta va al mazo del Jugador 2.
@@ -100,29 +85,62 @@ class JuegoGuerra:
                 '''
                 Reparte carta al Jugador 1.
                 '''
-                self.mazo_jugador1.agregar_carta(carta)
+                self.mazo_jugador1.agregar_carta(self.mazo.sacar_carta())
+                
                 
             if i%2 != 0:
                 '''
                 Reparte carta al Jugador 2.
                 '''
-                self.mazo_jugador2.agregar_carta(carta)
+                self.mazo_jugador2.agregar_carta(self.mazo.sacar_carta())
                 
-        return self.mazo_jugador1, self.mazo_jugador2
+                
     
     
     def iniciar_juego(self):
-        None
-    
+        
+        self.cantidad_turnos = 1000
+        
+        '''Se crea el mazo y se mezclan las cartas'''
+        self.crear_mazo()
+        
+        '''Se reparten las cartas a cada jugador'''
+        self.repartir()
+        
+        print(self.mazo_jugador1)
+        print(self.mazo_jugador2)
+        
+        # while self.turnos_jugados < self.cantidad_turnos:
+        #     try:
+        #         self.mostrar_juego()
+        #         self.turnos_jugados += 1
+            
+        #     except(ValueError):
+        #         pass
+            
+        #     if len(self.mazo_jugador1) == 0:
+        #         self.ganador = "Jugador 2"
+        #         print("Gana Jugador 2")
+        #         break
+            
+        #     elif len(self.mazo_jugador2) == 0:
+        #         self.ganador = "Jugador 1"
+        #         print("Gana Jugador 1")
+        #         break
+            
+        #     elif self.turnos_jugados == self.cantidad_turnos:
+        #         self.empate = True
+        #         print("Empate")
+        #         break
 
 
 # Pruebas locales
 
 if __name__ == "__main__":
     
-    juego1 = JuegoGuerra(22)
+    juego1 = JuegoGuerra()
     
-    print(juego1.crear_mazo())
+    juego1.crear_mazo()
     juego1.repartir()
     
     print("\nMazo:\n", juego1.mazo)
