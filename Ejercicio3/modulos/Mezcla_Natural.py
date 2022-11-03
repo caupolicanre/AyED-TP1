8fd8f5ede01cb2135eec8c5ff8076dbfd879fc7f
Slimerpy <tomybrehm29@gmail.com>
2022-11-02 23:22:10 -0300

    Documentacion

--- Ejercicio3/modulos/Mezcla_Natural.py
+++ Ejercicio3/modulos/Mezcla_Natural.py
@@ -10,7 +10,7 @@ with open("datos.txt") as datos, open("f1.txt","w") as f1, open("f2.txt","w")as 
         archis = mezclar_datos("./f1.txt", "./f2.txt", "./datos.txt")
         if archis == True:
             break
-with open("f1.txt","r")as f1:
-    with open("Mezclados.txt","w")as mezcla:
-        for linea in f1:
-            mezcla.write(linea)
+# with open("f1.txt","r")as f1:
+#     with open("Mezclados.txt","w")as mezcla:
+#         for linea in f1:
+#             mezcla.write(linea)
--- Ejercicio3/modulos/Mezclador.py
+++ Ejercicio3/modulos/Mezclador.py
@@ -13,15 +13,26 @@ def mezclar_datos(f1,f2,F):
                 f.write(aux) 
             return ordenada
         else:    
-            while d1 and d2:    #mientras existan datos en d1 y d2 
-                
+            while d1 and d2:    
+                '''
+                 Mientras existan datos en d1 y d2
+                '''
                 if d2 < d1:
+                    '''
+                    Si el dato(d2) en el archivo 2 es menor al dato(d1) en el archivo 1
+                    que se va a guardar el valor de d2 en una variable aux y se toma el siguiente valor
+                    si este valor es menor al anterior termina la sublista.
+                    '''
                     aux = d2
                     d2 = f2.readline()
                     if d2 < aux:
                         
                         sublistaf2 = False 
                 else:
+                    '''
+                    Se  guarda el valor de d1 en aux y se toma el siguiente valor
+                    si este valor es menor al anterior termina la sublista.
+                    '''
                     aux = d1
                     d1 = f1.readline()       
                     if d1 < aux:
@@ -29,6 +40,10 @@ def mezclar_datos(f1,f2,F):
                         sublistaf1 = False
                         
                 if sublistaf1 == False:
+                    '''
+                    Si se termino la sub lista escribir el resto de la sublista
+                    de f2 hasta que esta tambien se termine.
+                    '''
                     f.write(aux)
                     while aux < d2:
                         aux = d2
@@ -37,6 +52,10 @@ def mezclar_datos(f1,f2,F):
                     
                     
                 elif sublistaf2 == False:
+                    '''
+                    Si se termino la sublista en f1 escribir el resto de la sublista
+                    de el archivo2 hasta que esta tambien se termine
+                    '''
                     f.write(aux)
                     while aux < d1:
                         aux = d1
@@ -44,17 +63,30 @@ def mezclar_datos(f1,f2,F):
                         f.write(aux)
                     
                 if sublistaf1 != False and sublistaf2 != False:
+                    '''
+                    Actualizacion de las sublistas
+                    '''
                     f.write(aux)
                 else:
                     sublistaf1 = True    
                     sublistaf2 = True
                 
             while d1 == '' and d2 != '':  
+                '''
+                Si f2 se queda sin datos pero aun hay datos en f1
+                se agregan los datos restantes hasta que 
+                no haya mas datos en ninguno de los dos archivos.
+                '''
                 aux = d2
                 d2 = f2.readline()
                 f.write(aux)
                 
             while d2 == '' and d1 != '':
+                '''
+                Si f1 se queda sin datos pero aun hay datos en f1
+                se agregan los datos restantes hasta que 
+                no haya mas datos en ninguno de los dos archivos.
+                '''
                 aux = d1
                 d1 = f1.readline()
                 f.write(aux)
